b59845388f0f61839d4e7ce97ad207720a0950a0
Blacknibolion <35793362+blacknibolion@users.noreply.github.com>
2024-11-29 23:04:34 +0100

    challenge 2

---
+++ TestProject/Program.cs
@@ -0,0 +1,19 @@
+﻿string? readResult;
+string role = "";
+bool isValid = false;
+
+Console.WriteLine("Enter a role:");
+
+do{
+    
+    readResult = Console.ReadLine();
+    if(readResult != null){
+        role = readResult.Trim();
+    }
+    if (role == "admin" || role == "user" || role == "manager"){
+        isValid = true;
+    }else{
+        Console.WriteLine("Invalid role. Please enter a valid role.");
+    }
+} while (!isValid);
+Console.WriteLine($"You are a {role}.");
